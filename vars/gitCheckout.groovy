afcf68286fe235fdfec6be2fac8a6628cd444ff6
quanhao xu <xuqh1@yusys.com.cn>
2021-05-27 19:38:10 +0800

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,5 @@
 def call(Map config) {
-    timestamps {
         script {
             checkout([$class: 'GitSCM', branches: [[name: config.branch]], extensions: [[$class: 'CloneOption', depth: 1, noTags: false, reference: '', shallow: true], [$class: 'RelativeTargetDirectory', relativeTargetDir: config.subDir]], userRemoteConfigs: [[credentialsId: config.credentialsId, url: config.url]]])
         }
-    }
 }
