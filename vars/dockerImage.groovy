f9017a77424ce292668fdac7f6b66888b81bf5fd
quanhao xu <xuqh1@yusys.com.cn>
2021-05-28 13:51:20 +0800

    Update dockerImage.groovy

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -1,5 +1,6 @@
 def call(Map config) {
     node {
+        sh "echo ${config.password} | base64 -d | docker login -u ${config.username}  --password-stdin ${config.repo}"
         sh '''
         docker build -f config.dockerfile -t config.tag  config.context
         docker push config.tag
