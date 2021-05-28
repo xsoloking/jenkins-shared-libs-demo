8e17d0c9bb4d6611c95338ffcdaadad1a23504a3
quanhao xu <xuqh1@yusys.com.cn>
2021-05-28 13:13:25 +0800

    Update dockerImg.groovy

---
+++ vars/dockerImage.groovy
@@ -0,0 +1,8 @@
+def call(Map config) {
+    node {
+        sh '''
+        docker build -f config.dockerfile -t config.tag  config.context
+        docker push config.tag
+        '''
+    }
+}
--- vars/dockerImg.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map config) {
-    node {
-        sh '''
-        docker build -f ${config.dockerfile} -t ${config.tag}  ${config.context}
-        docker push ${config.tag}
-        '''
-    }
-}
