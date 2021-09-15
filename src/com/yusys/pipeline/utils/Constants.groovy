a00dd9377ff7eeb74ea59ee204eb3f2c4ba7768b
solo king <soloking.x@gmail.com>
2021-09-15 10:53:38 +0800

    bugfix for mq constants

--- src/com/yusys/pipeline/utils/Constants.groovy
+++ src/com/yusys/pipeline/utils/Constants.groovy
@@ -4,7 +4,7 @@ class Constants {
   public static final String RABBITMQ_EXCHANGE = "flow_tasks_results"
   public static final String RABBITMQ_START_ROUTING_KEY_LIVE = "started_live"
   public static final String RABBITMQ_END_ROUTING_KEY_LIVE = "end_live"
-  public static final String RABBITMQ_START_ROUTING_KEY_DEV = "started_DEV"
-  public static final String RABBITMQ_END_ROUTING_KEY_DEV = "end_DEV"
+  public static final String RABBITMQ_START_ROUTING_KEY_DEV = "started_dev"
+  public static final String RABBITMQ_END_ROUTING_KEY_DEV = "end_dev"
   public static final String RABBITMQ_NAME = "rabbitmq"
 }
